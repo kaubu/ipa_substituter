aaa700f967413b77764de8270a840e22d380ab28
kautionb <kaution.burrowes@gmail.com>
2022-01-28 19:10:37 +1300

    Add files via upload

--- plain_substituter.py
+++ plain_substituter.py
@@ -1,3 +1,5 @@
+# To just substitute anything
+
 import json
 
 debug = False
@@ -8,29 +10,28 @@ def sentence_capitalize(string1: str):
 	string2 = ". ".join(sentences2)
 	return string2
 
-print("""[IPA Substituter]
-1. Make a JSON file with the format 'ipa': 'substitution'
+print("""[Plain Substituter]
+1. Make a JSON file with the format 'to substitute': 'substitution'
 2. Load it here
 3. It will output""")
 
-ipa_dict_loc = input("Location of the JSON file: ")
-ipa_dict = None
+sub_dict_loc = input("Location of the JSON file: ")
+sub_dict = None
 
-with open(ipa_dict_loc, encoding="utf8") as dict_file:
-	ipa_dict = json.load(dict_file)
+with open(sub_dict_loc, encoding="utf8") as dict_file:
+	sub_dict = json.load(dict_file)
 
 while True:
 	# Repair the input
-	plain_input = input(">> ")
+	plain_input = input(">> ").lower()
 	plain_input = plain_input.replace("’", "'") # eng_to_ipa doesn't recognize ’s
 	
 	# Remove all ' and ˌ from it, stress markings
 	converted = plain_input.replace("ˈ", "")
 	converted = converted.replace("ˌ", "")
-	print(f"\nIPA: {converted}\n")
 
 	# Translate using the JSON file
-	for word, initial in ipa_dict.items():
+	for word, initial in sub_dict.items():
 		converted = converted.replace(word.lower(), initial)
 	
 	print(f"Converted: {converted}\n")
