7c516325f19d8b44730ff2fa7d1fae7497d86502
unknown <kaution.burrowes@gmail.com>
2023-06-22 19:45:26 +1200

    Update

--- ipa_substituter.py
+++ ipa_substituter.py
@@ -11,37 +11,46 @@ def sentence_capitalize(string1: str):
 	return string2
 
 print("""[IPA Substituter]
-1. Make a JSON file with the format 'ipa': 'substitution'
+1. Make a JSON file with the format 'ipa': 'substitution' (or vice versa)
 2. Load it here
 3. It will output""")
 
-ipa_dict_loc = input("Location of the JSON file: ")
+ipa_dict_loc = "./substitutions/" + input("Location of the JSON file (within 'substitutions/'): ")
 ipa_dict = None
 
 with open(ipa_dict_loc, encoding="utf8") as dict_file:
 	ipa_dict = json.load(dict_file)
 
+lang_to_ipa = False
+input_lang_to_ipa = input("Are you translating a non-English language into IPA (y/[n])? ").lower()
+
 capitalizations = False
-input_capitalization = input("Print captializations (y/[n])? ").lower()
-
-if input_capitalization == "yes" or input_capitalization == "y":
-	capitalizations = True
-
 stresses = False
-input_stresse = input("Print stresses (y/[n])? ").lower()
+show_ipa = False
 
-if input_stresse == "yes" or input_stresse == "y":
-	stresses = True
+if input_lang_to_ipa == "yes" or input_lang_to_ipa == "y":
+	lang_to_ipa = True
+else:
+	show_ipa = True
 
-show_ipa = True
-input_show_ipa = input("Print IPA ([y]/n)? ").lower()
+	input_capitalization = input("Print captializations (y/[n])? ").lower()
 
-if input_show_ipa == "no" or input_show_ipa == "n":
-	show_ipa = False
+	if input_capitalization == "yes" or input_capitalization == "y":
+		capitalizations = True
+
+	input_stresses = input("Print stresses (y/[n])? ").lower()
+
+	if input_stresses == "yes" or input_stresses == "y":
+		stresses = True
+
+	input_show_ipa = input("Print IPA ([y]/n)? ").lower()
+
+	if input_show_ipa == "no" or input_show_ipa == "n":
+		show_ipa = False
 
 while True:
 	# Repair the input
-	ipa_input = input(">> ")
+	ipa_input = input(">> ").lower()
 	if ipa_input == "" or ipa_input == None: # It will error otherwise
 		print()
 		continue
@@ -57,11 +66,18 @@ while True:
 		converted = converted.replace("ˈ", "")
 		converted = converted.replace("ˌ", "")
 	
-	print(f"\nIPA: {converted}\n")
+	if show_ipa: print(f"\nIPA: {converted}\n")
 
 	# Translate using the JSON file
 	for word, initial in ipa_dict.items():
 		converted = converted.replace(word.lower(), initial)
 	
-	print(f"Converted: {converted}\n")
-	if capitalizations: print(f"Capitalized: {sentence_capitalize(converted)}\n")
+	# Print out the original translated
+	for word, initial in ipa_dict.items():
+		ipa_input = ipa_input.replace(word.lower(), initial)
+	
+	if lang_to_ipa:
+		print(f"Result: {ipa_input}")
+	else:
+		print(f"Converted: {converted}\n")
+		if capitalizations: print(f"Capitalized: {sentence_capitalize(converted)}\n")
